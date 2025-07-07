e357e661d9b4aabfd5e55d22f6917f863d0a7077
Roy <arindamroy11235@gmail.com>
2025-07-07 13:03:14 +0000

    added try-catch logic to handler

--- handler.py
+++ handler.py
@@ -1,14 +1,33 @@
+import os
+
 import runpod
-from utils import get_views_aspects
+from utils import get_views_aspects, get_views_aspects_fallback
+
 
 def handler(event):
     input = event["input"]
     segment_ids = [str(segment_id) for segment_id in input["segment_ids"]]
     user_prompt = input["user_prompt"]
     response_language = input["response_language"]
-    
-    response = get_views_aspects(segment_ids, user_prompt, response_language)
+    if input.get("run_fallback", False) or os.getenv("RUN_FALLBACK", "false").lower() == "true":
+        RUN_FALLBACK_BY_DEFAULT = True
+    else:
+        RUN_FALLBACK_BY_DEFAULT = False
 
-    return response
+    if RUN_FALLBACK_BY_DEFAULT:
+        response = get_views_aspects_fallback(segment_ids, user_prompt, response_language)
+        return response
+    try:
+        response = get_views_aspects(segment_ids, user_prompt, response_language)
+    except Exception as e:
+        print(f"Error in default get_views_aspects: {e}; falling back to get_views_aspects_fallback")
+        try: 
+            response = get_views_aspects_fallback(segment_ids, user_prompt, response_language)
+            return response
+        except Exception as e:
+            print(f"Error in get_views_aspects_fallback: {e}")
+            raise e
+            
+
 
 runpod.serverless.start({"handler": handler})
