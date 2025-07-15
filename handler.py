08525c13c1d52ff86d192b55d20f50a1c178221a
Roy <arindamroy11235@gmail.com>
2025-07-15 10:38:43 +0000

    Enhance handler and utils to incorporate project analysis run ID; update response handling and Directus integration for improved tracking and error management.

--- handler.py
+++ handler.py
@@ -9,25 +9,31 @@ def handler(event):
     segment_ids = [str(segment_id) for segment_id in input["segment_ids"]]
     user_prompt = input["user_prompt"]
     response_language = input["response_language"]
+    project_analysis_run_id = input["project_analysis_run_id"]
     if input.get("run_fallback", False) or os.getenv("RUN_FALLBACK", "false").lower() == "true":
         RUN_FALLBACK_BY_DEFAULT = True
     else:
         RUN_FALLBACK_BY_DEFAULT = False
 
     if RUN_FALLBACK_BY_DEFAULT:
-        response = get_views_aspects_fallback(segment_ids, user_prompt, response_language)
+        response = get_views_aspects_fallback(
+            segment_ids, user_prompt, project_analysis_run_id, response_language
+        )
         return response
     try:
-        response = get_views_aspects(segment_ids, user_prompt, response_language)
+        response = get_views_aspects(
+            segment_ids, user_prompt, project_analysis_run_id, response_language
+        )
     except Exception as e:
-        print(f"Error in default get_views_aspects: {e}; falling back to get_views_aspects_fallback")
-        try: 
+        print(
+            f"Error in default get_views_aspects: {e}; falling back to get_views_aspects_fallback"
+        )
+        try:
             response = get_views_aspects_fallback(segment_ids, user_prompt, response_language)
             return response
         except Exception as e:
             print(f"Error in get_views_aspects_fallback: {e}")
             raise e
-            
 
 
 runpod.serverless.start({"handler": handler})
